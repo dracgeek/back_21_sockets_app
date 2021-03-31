63d1aa78fb3954bf387258d7c03103c47759066a
Wolfgang Schubert <david.rodriguez@agileinnova.org>
2021-03-31 15:04:27 -0500

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', 4500);
+app.set('port', 8001);
 app.use(express.static(path.join(__dirname,'public')));
 
 const server = app.listen(app.get('port'),()=>{
